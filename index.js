40e50ea1ed2c41342483127b2da708954694b8ab
alinOrwls <71549673+alinOrwls@users.noreply.github.com>
2020-10-04 11:16:37 +0100

    Icon set

--- index.js
+++ index.js
@@ -35,16 +35,17 @@ function displayWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let feelsElement = document.querySelector("#feels");
   let dateElement = document.querySelector("#date");
-  //  let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   feelsElement.innerHTML = Math.round(response.data.main.feels_like);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  // iconElement.setAttribute(
-  // "src",
-  // `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 
   let fahrenheitElement = document.querySelector("#fahrenheit");
   fahrenheitElement.innerHTML = Math.round(
@@ -65,13 +66,5 @@ function formatForm(event) {
 }
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", formatForm);
-
-function inFahrenheit() {
-  let fahrenheitElement = document.querySelector("#fahrenheit");
-  let form = (temperatureElement.innerHTML * 9) / 5 + 32;
-  fahrenheitElement.innerHTML = form;
-}
-let temperatureElement = document.querySelector("#temperature");
-inFahrenheit(temperature.innerHTML);
 
 search("Tokyo");
